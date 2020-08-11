89e93b78a869f8298b7b0361575fc09ad81f3061
miwebst <50640090+miwebst@users.noreply.github.com>
2020-08-10 17:02:58 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -1,7 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    // You can call and await an async method here
-    return {
-        body: req.headers
+    context.res = {
+        body: req
     };
 }
