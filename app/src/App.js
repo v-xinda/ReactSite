d21187d90cc30e88440fd0d1764e4a77e14dad27
Nathan <naturins@microsoft.com>
2023-03-22 23:49:49 -0700

    Commit made from VS Code Azure Static Web Apps

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Look at this really cool spinny thing we have here. Visible update. Hello from the Azure Tools for VS Code team!
+          Look at this really cool spinny thing we have here.
         </p>
         <a
           className="App-link"
